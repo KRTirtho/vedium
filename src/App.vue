93d744202c598a7010813f01bd548aa8fbbc9721
KR Tirtho <krtirtho@gmail.com>
2021-08-03 07:55:59 +0000

    before adding vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,14 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
+import { defineComponent } from "vue";
 
 export default defineComponent({
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-})
+  name: "App",
+  components: {},
+});
 </script>
 
 <style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import TsChecker from "vite-plugin-ts-checker";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
-})
+  plugins: [vue(), TsChecker({ checker: "vue-tsc" })],
+  server: {
+    hmr: {
+      port: 443,
+    },
+  },
+});
